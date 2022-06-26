6f8c6494608200e0acece35a5b80cf2955eb242b
ujeongoh <yujoh94@gmail.com>
2022-06-26 21:49:00 +0900

    initial commit

---
+++ ddang/src/main/java/com/landlord/ddang/DdangApplication.java
@@ -0,0 +1,16 @@
+package com.landlord.ddang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdangApplication.class, args);
+		System.out.println("Hello World");
+
+
+	}
+
+}
